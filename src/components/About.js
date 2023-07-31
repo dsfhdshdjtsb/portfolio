84fec220a79af57af520f531aac70bae2421b60d
Nicholas Suh <nicksuh17@gmail.com>
2023-07-30 17:13:24 -0700

    using pos fixed instead of parallax

---
+++ src/components/About.js
@@ -0,0 +1,50 @@
+import CSSIcon from "../images/CSSIcon.png"
+import HTMLIcon from "../images/HTMLIcon.png"
+import GitIcon from "../images/GitIcon.png"
+import JavaIcon from "../images/JavaIcon.png"
+import JavascriptIcon from "../images/JavascriptIcon.png"
+import ReactIcon from "../images/ReactIcon.png"
+import Instagram from "../images/Instagram.png"
+import Discord from "../images/Discord.png"
+import Github from "../images/Github.png"
+import Linkedin from "../images/Linkedin.png"
+import Outlook from "../images/Outlook.png"
+import portrait from "../images/portrait.jpg"
+
+export default function About(){
+    return(
+        <div className="About">
+            
+            <div className="Content">
+                <h2>About me</h2>
+                <p>I am a first year student studying Computer Science. I Have a passion
+                for game design, web development, web3, and machine learning. Basically anything
+                related to programming</p>
+                
+
+                <h2>My Skills</h2>
+                <div className="LogoImages">
+                    <img src={JavaIcon} id="javaIcon"></img>
+                    <img src={HTMLIcon}></img>
+                    <img src={CSSIcon}></img>
+                    <div id= "lineBreak"></div>
+                    <img src={JavascriptIcon}></img>
+                    <img src={ReactIcon}></img>
+                    <img src={GitIcon}></img>
+                </div>
+            </div>
+            <div className="imageAndSocials">
+                <img id="portrait" src= {portrait}></img>
+                <div className="Socials">
+                    <a href='https://github.com/dsfhdshdjtsb' target="_blank"><img src={Github}/></a>
+                    <a href='https://www.linkedin.com/in/nicholas-suh-5ba013274/' target="_blank"><img src={Linkedin}/></a>
+                    <img src={Outlook} onClick={()=>window.location = 'mailto:nicksuh@gatech.edu'}/>
+                    <a href='https://www.instagram.com/nick_suh/' target="_blank"><img src={Instagram}/></a>
+                    <a href='https://discordapp.com/users/395005138000936960' target="_blank"><img src={Discord}/></a>
+                </div>
+            </div>
+
+            
+        </div>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+        <div className="Header">
+            <div className="Text">
+                <h1>Hey, I'm Nick Suh</h1>
+                <p>CS major at Georgia Tech</p>
+            </div>
+        </div>
+    )
+}
