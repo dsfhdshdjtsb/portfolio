3b187fae13509fe8b287a79367785f83f10f720a
Nicholas Suh <nicksuh17@gmail.com>
2023-07-30 18:15:19 -0700

    typewriter effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
+import Typed from 'typed.js';
+import React from "react";
 export default function Header(){
+    const el = React.useRef(null);
+
+    React.useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ['CS major at Georgia Tech^5000', 'programmer^1000', 'web developer^1000', 'minecraft modder^1000'],
+            typeSpeed: 50,
+            loop: true
+        });
+
+        return () => {
+        // Destroy Typed instance during cleanup to stop animation
+            typed.destroy();
+        };
+    }, []);
+
     return(
         <div className="Header">
             <div className="Text">
                 <h1>Hey, I'm Nick Suh</h1>
-                <p>CS major at Georgia Tech</p>
+                <div className="Container">
+                    <span>I'm a </span><span ref={el}></span>
+                </div>
+                
             </div>
         </div>
     )
