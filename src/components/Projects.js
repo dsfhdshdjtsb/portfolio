851d0988faf160d4ab90eb16630f075b61370413
Nicholas Suh <nicksuh17@gmail.com>
2023-07-31 00:19:24 -0700

    no carosel version

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,68 +1,37 @@
 import Curseforge from "../images/Curseforge.png"
 import RoadCode from "../images/Roadcode.png"
 import Metruhnome from "../images/Metruhnome.png"
+import Portfolio from "../images/Portfolio.png"
 
 export default function Projects(){
     return(
         <div className="Projects">
             <h2>My Projects</h2>
-            <div className="Slider">
-                <div className="SlideTrack">
+            <div className="Images">
+                <div className="Slide">
+                    <img className="cenchantsImg" src={Curseforge}/>
+                    <div class="overlay">
+                        <div class="text">Minecraft Modding</div>
+                    </div>
+                </div>
+                <div className="Slide">
+                    <img className="roadcodeImg" src={RoadCode}/>
+                    <div class="overlay">
+                        <div class="text">Roadcode</div>
+                    </div>
+                </div>
+                <div className="Slide">
+                    <img className="portfolioImg" src={Portfolio}/>
+                    <div class="overlay">
+                        <div class="text">Portfolio Website</div>
+                    </div>
+                </div>
+                <div className="Slide">
+                    <img className="Metruhnome" src={Metruhnome}/>
 
-                    <div className="Slide">
-                        <img className="cenchantsImg" src={Curseforge}/>
-                        <div class="overlay">
-                            <div class="text">Combat Enchantments</div>
-                        </div>
+                    <div class="overlay">
+                        <div class="text">Metruhnome</div>
                     </div>
-                    <div className="Slide">
-                        <img className="roadcodeImg" src={RoadCode}/>
-                        <div class="overlay">
-                            <div class="text">Roadcode</div>
-                        </div>
-                    </div>
-                    <div className="Slide">
-                        <img className="aabilitiesImg" src={Curseforge}/>
-                        <div class="overlay">
-                            <div class="text">Armor Abilities</div>
-                        </div>
-                    </div>
-                    <div className="Slide">
-                        <img className="Metruhnome" src={Metruhnome}/>
-                        <div class="overlay">
-                            <div class="text">Metruhnome</div>
-                        </div>
-                    </div>
-
-                    
-                    <div className="Slide">
-                        <img className="cenchantsImg" src={Curseforge}/>
-                        <div class="overlay">
-                            <div class="text">Combat Enchantments</div>
-                        </div>
-                    </div>
-                    <div className="Slide">
-                        <img className="roadcodeImg" src={RoadCode}/>
-                        <div class="overlay">
-                            <div class="text">Roadcode</div>
-                        </div>
-                    </div>
-                    <div className="Slide">
-                        <img className="aabilitiesImg" src={Curseforge}/>
-                        <div class="overlay">
-                            <div class="text">Armor Abilities</div>
-                        </div>
-                    </div>
-                    <div className="Slide">
-                        <img className="Metruhnome" src={Metruhnome}/>
-                        <div class="overlay">
-                            <div class="text">Metruhnome</div>
-                        </div>
-                    </div>
-                    
-                    
-                    
-                    
                 </div>
                 
             </div>
