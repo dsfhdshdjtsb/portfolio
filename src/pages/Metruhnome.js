f7aa806e5d75d1000df0a1e726bcaa1d582a6165
Nicholas Suh <nicksuh17@gmail.com>
2023-08-07 15:38:33 -0700

    typo

--- src/pages/Metruhnome.js
+++ src/pages/Metruhnome.js
@@ -17,7 +17,7 @@ export default function Metruhnome(){
             <ProjectEntry title="MetruhnomeEntry" name="A Syncronized Metronome" roles="Backend Dev"
                 technologies="Javascript, HTML, CSS, NTP, Node.js," dates="May-September 2022" github="https://github.com/dsfhdshdjtsb/synced-metronome"
                 project="http://www.metruhnome.com/" desc="Metruhnome is a web app that allows users to syncronize their metronomes. Simply enter the BPM you want to play at, 
-                have other users enter the room code, and Metruhnome will syncronize your metronome with everyone else's. Originally an idea given to me by my band director,
+                have other users enter the room code, and Metruhnome will synchronize  your metronome with everyone else's. Originally an idea given to me by my band director,
                 Metruhnome was created for my high school's music department and was used in both full orchestra and small group settings. Metruhnome uses vanilla HTML and
                 CSS for the frontend, and Javascript along with node.js for the backend. The backend uses Network Time Protocol (NTP) to sync the metronomes, although syncronization
                 is subject to ping and latency spikes. Metruhnome was developed in conjunction with 1 other student."
