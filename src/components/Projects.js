1a9b468a713ab6f59b9f98dcd194d011281ae147
Nicholas Suh <nicksuh17@gmail.com>
2024-02-01 00:54:33 -0500

    fmotion soon

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,28 +11,30 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 export default function Projects(){
     return(
         
-            <div className="Projects" >
-                <div>
+            <div className="Projects">
+                
+                    <h2 id="ProjectsTitle" style={{textAlign: "left"}}>My Projects</h2>
+                    <div id="PFrameWrapper">
+                        <div className="ProjectsWrapper">
+                            <Project title="Minecraft Modding • Creator" date1="August 2021 - Present" subh="" desc="With over 200,000 total downloads, Combat Enchantments is my most downloaded mod and showcases my ability to successfully handle large, longterm projects. Written entirely in Java and utilizing the Fabric modloader, Combat Enchantments introduces more than 30 meticulously balanced enchantments, enhancing the game's combat system without alienating veteran players."/>
+                            <Project title="Minecraft Modding" subh="Creator" desc="AOSIJFOAIJFO IASJDOFI JASEOFIJASOIFJASO EFIEJSO EFIFJO EFIFJSO EFIJS EOFIJ"/>
+                            <Project title="Minecraft Modding" subh="Creator" desc="AOSIJFOAIJFO IASJDOFI JASEOFIJASOIFJASO EFIEJSO EFIFJO EFIFJSO EFIJS EOFIJ"/>
+                            <Project title="Minecraft Modding" subh="Creator" desc="AOSIJFOAIJFO IASJDOFI JASEOFIJASOIFJASO EFIEJSO EFIFJO EFIFJSO EFIJS EOFIJ"/>
+                        </div>
+                        <div id="FrameWrapper">
+                            <div id="BigPictureFrame">
+                            </div>
+                            <div id="SmallFrameWrapper">
+                                <div id="MedPictureFrame">
+                                </div>
+                                <div id="SmallPictureFrame">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     
-                </div>
-                <h2 id="ProjectsTitle" style={{textAlign: "left"}}>My Projects</h2>
-                <div className="ProjectsWrapper" >
-                    <Project title="Minecraft Modding • Creator" date1="August 2021 - Present" subh="" desc="With over 200,000 total downloads, Combat Enchantments is my most downloaded mod and showcases my ability to successfully handle large, longterm projects. Written entirely in Java and utilizing the Fabric modloader, Combat Enchantments introduces more than 30 meticulously balanced enchantments, enhancing the game's combat system without alienating veteran players."/>
-                    <Project title="Minecraft Modding" subh="Creator" desc="AOSIJFOAIJFO IASJDOFI JASEOFIJASOIFJASO EFIEJSO EFIFJO EFIFJSO EFIJS EOFIJ"/>
-                    <Project title="Minecraft Modding" subh="Creator" desc="AOSIJFOAIJFO IASJDOFI JASEOFIJASOIFJASO EFIEJSO EFIFJO EFIFJSO EFIJS EOFIJ"/>
-                    <Project title="Minecraft Modding" subh="Creator" desc="AOSIJFOAIJFO IASJDOFI JASEOFIJASOIFJASO EFIEJSO EFIFJO EFIFJSO EFIJS EOFIJ"/>
-                </div>
-                <div id="FrameWrapper">
-                    hi
-                    <div id="BigPictureFrame">
-                    </div>
-                    <div id="SmallFrameWrapper">
-                        <div id="MedPictureFrame">
-                        </div>
-                        <div id="SmallPictureFrame">
-                        </div>
-                    </div>
-                </div>
+                
+                
                 
             </div>
         
