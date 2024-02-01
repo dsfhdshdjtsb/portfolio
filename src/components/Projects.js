734b2db5c36f6e3141a9ce22ce1e1ab63b97d78b
Nicholas Suh <nicksuh17@gmail.com>
2024-02-01 10:15:30 -0500

    sticky

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,14 +2,36 @@ import Curseforge from "../images/Curseforge.png"
 import RoadCode from "../images/Roadcode.png"
 import Metruhnome from "../images/Metruhnome.png"
 import Portfolio from "../images/Portfolio.png"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Project from "./Project"
 import TherapalImg from "../images/Therapal.jpg"
 import AvImg from "../images/Av.png"
+import $ from 'jquery';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { motion, useScroll } from "framer-motion"
 
 export default function Projects(){
-    return(
+    const { scrollYProgress } = useScroll();
+    const [imagesSticky, setImagesSticky] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            console.log(scrollYProgress);
+            window.addEventListener('scroll', () => {
+                document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+            }, false);
+        }); 
+        var pos = $('#FrameWrapper').offset();
+        $(window).scroll(function(){
+            console.log(imagesSticky);
+            if ($(window).scrollTop() > pos.top) {
+                setImagesSticky(true);
+            } else {
+                setImagesSticky(false);
+            }
+        });
+    }, []);
+    return( 
         
             <div className="Projects">
                 
@@ -21,19 +43,21 @@ export default function Projects(){
                             <Project title="Minecraft Modding" subh="Creator" desc="AOSIJFOAIJFO IASJDOFI JASEOFIJASOIFJASO EFIEJSO EFIFJO EFIFJSO EFIJS EOFIJ"/>
                             <Project title="Minecraft Modding" subh="Creator" desc="AOSIJFOAIJFO IASJDOFI JASEOFIJASOIFJASO EFIEJSO EFIFJO EFIFJSO EFIJS EOFIJ"/>
                         </div>
-                        <div id="FrameWrapper">
-                            <div id="BigPictureFrame">
-                            </div>
-                            <div id="SmallFrameWrapper">
-                                <div id="MedPictureFrame">
+                        <div id="FrameWrapper" >
+                            <div id="Frame" >
+                                {/* <div id="BigPictureFrame">
                                 </div>
-                                <div id="SmallPictureFrame">
-                                </div>
+                                <div id="SmallFrameWrapper">
+                                    <div id="MedPictureFrame">
+                                    </div>
+                                    <div id="SmallPictureFrame">
+                                        
+                                    </div>
+                                </div> */}
                             </div>
                         </div>
-                    </div>
-                    
-                
+                       
+                    </div>                
                 
                 
             </div>
